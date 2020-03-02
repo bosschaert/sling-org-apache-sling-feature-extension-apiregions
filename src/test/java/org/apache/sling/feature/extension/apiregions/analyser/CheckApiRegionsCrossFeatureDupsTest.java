f0dff51342fe046026c48a9dd5ba1779e6467034
David Bosschaert <bosschae@adobe.com>
2020-03-02 14:54:39 +0000

    Unit tests

--- src/test/java/org/apache/sling/feature/extension/apiregions/analyser/CheckApiRegionsCrossFeatureDupsTest.java
+++ src/test/java/org/apache/sling/feature/extension/apiregions/analyser/CheckApiRegionsCrossFeatureDupsTest.java
@@ -34,6 +34,8 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -48,7 +50,10 @@ public class CheckApiRegionsCrossFeatureDupsTest {
 
         Scanner scanner = getScanner();
         AnalyserTask at = new CheckApiRegionsCrossFeatureDups();
-        Analyser a = new Analyser(scanner, at);
+        Map<String, Map<String,String>> configs =
+                Collections.singletonMap("api-regions-crossfeature-dups",
+                Collections.singletonMap("warningPackages", "x.y.z"));
+        Analyser a = new Analyser(scanner, configs, at);
         AnalyserResult res = a.analyse(f);
         assertEquals(1, res.getErrors().size());
         assertEquals(0, res.getWarnings().size());
@@ -57,6 +62,114 @@ public class CheckApiRegionsCrossFeatureDupsTest {
         assertTrue(err.contains("a.b.c"));
         assertTrue(err.contains("g:f3:1"));
         assertTrue(err.contains("feature-export2"));
+    }
+
+    @Test
+    public void testOverlapWarning() throws Exception {
+        Path fp = new File(getClass().getResource("/crossfeatdups/fm1.json").getFile()).toPath();
+        String fm = new String(Files.readAllBytes(fp));
+
+        Feature f = FeatureJSONReader.read(new StringReader(fm), null);
+
+        Scanner scanner = getScanner();
+        CheckApiRegionsCrossFeatureDups at = new CheckApiRegionsCrossFeatureDups();
+        Map<String, String> cfg = new HashMap<>();
+        cfg.put("warningPackages", "a.b.c");
+        cfg.put("ignoredPackages", "x.y.z");
+        Map<String, Map<String,String>> configs =
+            Collections.singletonMap("api-regions-crossfeature-dups", cfg);
+        Analyser a = new Analyser(scanner, configs, at);
+        AnalyserResult res = a.analyse(f);
+        assertEquals(0, res.getErrors().size());
+        assertEquals(1, res.getWarnings().size());
+
+        String err = res.getWarnings().get(0);
+        assertTrue(err.contains("a.b.c"));
+        assertTrue(err.contains("g:f3:1"));
+        assertTrue(err.contains("feature-export2"));
+    }
+
+    @Test
+    public void testOverlapWarning2() throws Exception {
+        Path fp = new File(getClass().getResource("/crossfeatdups/fm1.json").getFile()).toPath();
+        String fm = new String(Files.readAllBytes(fp));
+
+        Feature f = FeatureJSONReader.read(new StringReader(fm), null);
+
+        Scanner scanner = getScanner();
+        CheckApiRegionsCrossFeatureDups at = new CheckApiRegionsCrossFeatureDups();
+        Map<String, String> cfg = new HashMap<>();
+        cfg.put("warningPackages", "x.*, a.*");
+        Map<String, Map<String,String>> configs =
+            Collections.singletonMap("api-regions-crossfeature-dups", cfg);
+        Analyser a = new Analyser(scanner, configs, at);
+        AnalyserResult res = a.analyse(f);
+        assertEquals(0, res.getErrors().size());
+        assertEquals(1, res.getWarnings().size());
+
+        String err = res.getWarnings().get(0);
+        assertTrue(err.contains("a.b.c"));
+        assertTrue(err.contains("g:f3:1"));
+        assertTrue(err.contains("feature-export2"));
+    }
+
+    @Test
+    public void testOverlapIgnore() throws Exception {
+        Path fp = new File(getClass().getResource("/crossfeatdups/fm1.json").getFile()).toPath();
+        String fm = new String(Files.readAllBytes(fp));
+
+        Feature f = FeatureJSONReader.read(new StringReader(fm), null);
+
+        Scanner scanner = getScanner();
+        CheckApiRegionsCrossFeatureDups at = new CheckApiRegionsCrossFeatureDups();
+        Map<String, String> cfg = new HashMap<>();
+        cfg.put("ignoredPackages", "a.b.c");
+        Map<String, Map<String,String>> configs =
+            Collections.singletonMap("api-regions-crossfeature-dups", cfg);
+        Analyser a = new Analyser(scanner, configs, at);
+        AnalyserResult res = a.analyse(f);
+        assertEquals(0, res.getErrors().size());
+        assertEquals(0, res.getWarnings().size());
+    }
+
+    @Test
+    public void testOverlapError2() throws Exception {
+        Path fp = new File(getClass().getResource("/crossfeatdups/fm1.json").getFile()).toPath();
+        String fm = new String(Files.readAllBytes(fp));
+
+        Feature f = FeatureJSONReader.read(new StringReader(fm), null);
+
+        Scanner scanner = getScanner2();
+        AnalyserTask at = new CheckApiRegionsCrossFeatureDups();
+        Analyser a = new Analyser(scanner, at);
+        AnalyserResult res = a.analyse(f);
+        assertEquals(2, res.getErrors().size());
+        assertEquals(0, res.getWarnings().size());
+
+        String allErrs = res.getErrors().get(0) + res.getErrors().get(1);
+        assertTrue(allErrs.contains("a.b.c"));
+        assertTrue(allErrs.contains("zzz.zzz"));
+    }
+
+    @Test
+    public void testOverlapErrorOnlyGlobal() throws Exception {
+        Path fp = new File(getClass().getResource("/crossfeatdups/fm1.json").getFile()).toPath();
+        String fm = new String(Files.readAllBytes(fp));
+
+        Feature f = FeatureJSONReader.read(new StringReader(fm), null);
+
+        Scanner scanner = getScanner2();
+        AnalyserTask at = new CheckApiRegionsCrossFeatureDups();
+        Map<String, Map<String,String>> configs =
+                Collections.singletonMap("api-regions-crossfeature-dups",
+                Collections.singletonMap("regions", "something,global"));
+        Analyser a = new Analyser(scanner, configs, at);
+        AnalyserResult res = a.analyse(f);
+        assertEquals(1, res.getErrors().size());
+        assertEquals(0, res.getWarnings().size());
+
+        String err = res.getErrors().get(0);
+        assertTrue(err.contains("zzz.zzz"));
     }
 
     private Scanner getScanner() throws IOException {
@@ -70,6 +183,29 @@ public class CheckApiRegionsCrossFeatureDupsTest {
                     return getClass().getResource("/crossfeatdups/test-bundles/feature-export2.jar");
                 case "g:noexp:1":
                     return getClass().getResource("/crossfeatdups/test-bundles/no-exports.jar");
+                case "g:extra:1":
+                    return getClass().getResource("/crossfeatdups/test-bundles/no-exports.jar");
+                }
+                return null;
+            }
+        };
+        Scanner scanner = new Scanner(ap, Collections.singletonList(new ApiRegionsExtensionScanner()), Collections.emptyList());
+        return scanner;
+    }
+
+    private Scanner getScanner2() throws IOException {
+        ArtifactProvider ap = new ArtifactProvider() {
+            @Override
+            public URL provide(ArtifactId id) {
+                switch (id.toMvnId()) {
+                case "g:exp1:1":
+                    return getClass().getResource("/crossfeatdups/test-bundles/feature-export.jar");
+                case "g:exp2:1":
+                    return getClass().getResource("/crossfeatdups/test-bundles/feature-export2.jar");
+                case "g:noexp:1":
+                    return getClass().getResource("/crossfeatdups/test-bundles/no-exports.jar");
+                case "g:extra:1":
+                    return getClass().getResource("/crossfeatdups/test-bundles/feature-export3.jar");
                 }
                 return null;
             }
